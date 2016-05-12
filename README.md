6a4526529ab4d05b4ebef4ac488cddb32dd0e7ed
Mejdi Ourir <m.ourir@ivsweb.com>
2016-05-13 01:06:17 +0200

    first version

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # dispatcher
+
+Used to broadcast payloads to registered callbacks. 
+
+# API
+- **register(function callback): string**
+Registers a callback. Returns id that can be unregist.
+
+- **unregister(string id): void**
+Removes a callback based on its id.
+
+- **dispatch(object payload): void** 
+Dispatches a payload to all registered callbacks.
---
+++ index.js
@@ -0,0 +1,38 @@
+const _prefix = 'ID_';
+
+class Dispatcher{
+
+  constructor() {
+    this._callbacks = {};
+  }
+
+  /**
+   * @param callback : (payload : <payload>) => void
+   * @returns {number}
+   */
+  register(callback){
+    var id = _prefix + this._lastID++;
+    this._callbacks[id] = callback;
+    return id;
+  }
+
+  /**
+   * @param {number}
+   */
+  unregister(id){
+    delete this._callbacks[id];
+  }
+
+  /**
+   * @param payload : <payload>
+   */
+  dispatch(payload){
+    this._startDispatching(payload);
+    for (var id in this._callbacks) {
+      this._callbacks[id](payload);
+    }
+  }
+}
+
+
+module.exports = Dispatcher;
